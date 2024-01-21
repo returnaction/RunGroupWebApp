85892e463bb253c2030d1f3ebfc49b5e0b1e1ac6
returnaction <obergannv@gmail.com>
2024-01-21 01:19:06 -0500

    Login Part is done

---
+++ RunGroupWebApp/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Models;
+using RunGroupWebApp.ViewModels;
+using System.Runtime.CompilerServices;
+
+namespace RunGroupWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            LoginViewModel response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid) return View();
+
+            AppUser? user = await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
+
+            if(user != null)
+            {   
+                // user is found, check password
+                bool passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+                if (passwordCheck)
+                {
+                    // password correct, sign in
+                    var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Race");
+                    }
+                }
+
+                // password is incorrect
+                TempData["Error"] = "Wrong credentials. Please, try again";
+                return View(loginViewModel);
+            }
+            // User not found
+            TempData["Error"] = "wrong credentials. Please try again";
+            return View(loginViewModel);
+
+
+        }
+    }
+}
--- RunGroupWebApp/Program.cs
+++ RunGroupWebApp/Program.cs
@@ -32,7 +32,7 @@ var app = builder.Build();
 
 if(args.Length == 1 && args[0].ToLower() == "seeddata")
 {
-    await Seed.SeedUsersAndRolesAsync(app);
+    //await Seed.SeedUsersAndRolesAsync(app);
     //Seed.SeedData(app);
 }
 
@@ -40,7 +40,6 @@ if(args.Length == 1 && args[0].ToLower() == "seeddata")
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
